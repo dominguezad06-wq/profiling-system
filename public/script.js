0520da3c103a9c34f7e20c69f53e6cbd72e0f3cf
dominguezad06-wq <dominguezad06@gmail.com>
2026-04-06 11:08:48 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -27,7 +27,7 @@ function formatDate(dateStr) {
 
 // Show Forms
 function showResidentForm(){ document.getElementById('login-page').style.display='none'; document.getElementById('resident-form').style.display='block'; }
-function showLogin(){ document.getElementById('login-page').style.display='block'; document.getElementById('resident-form').style.display='none'; document.getElementById('forgot-page').style.display='none'; }
+function showLogin(){ document.getElementById('login-page').style.display='flex'; document.getElementById('resident-form').style.display='none'; document.getElementById('forgot-page').style.display='none'; }
 function showForgotPassword(){ document.getElementById('login-page').style.display='none'; document.getElementById('forgot-page').style.display='flex'; }
 function sendOTP() {
   const email = document.getElementById('forgot-email').value;
